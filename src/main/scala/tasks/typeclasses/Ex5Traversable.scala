2d5e2b5b32ab4d8f1f06e737ac36d0060b1a6ed8
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-03-30 19:39:46 +0200

    Task5

--- src/main/scala/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,9 @@
 package u04lab
+
 import u03.Sequences.* 
 import Sequence.*
+import u03.Optionals.*
+import Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -17,10 +20,37 @@ import Sequence.*
 
 object Ex5Traversable:
 
-  def log[A](a: A): Unit = println("The next element is: "+a)
+  def log[A](a: A): Unit = println("The next element is: " + a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logAll[T[_]: Traversable, A](t: T[A])(f: A => Unit): Unit =
+    summon[Traversable[T]].consume(t)(f)
 
-  
+  trait Traversable[T[_]]:
+    def consume[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Optional] with
+    def consume[A](o: Optional[A])(f: A => Unit): Unit = o match
+      case Just(a) => f(a)
+      case _ => ()
+
+  given Traversable[Sequence] with
+    def consume[A](s: Sequence[A])(f: A => Unit): Unit = s match
+      case Cons(h, t) => f(h); consume(t)(f)
+      case _ => ()
+
+  @main def tryTraversable =
+    val o1 = Just(5)
+    logAll(o1)(log)
+    logAll(o1)(println)
+
+    val o2 = Empty()
+    logAll(o2)(log)
+    logAll(o2)(println)
+
+    val s1 = Cons(10, Cons(20, Cons(30, Nil())))
+    logAll(s1)(log)
+    logAll(s1)(println)
+
+    val s2 = Cons("a", Cons("b", Cons("c", Nil())))
+    logAll(s2)(log)
+    logAll(s2)(println)
